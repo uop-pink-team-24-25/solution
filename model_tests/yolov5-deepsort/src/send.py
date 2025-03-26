232a144a0d47446f64b2ab8b6c99e6d4d3238915
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2025-03-26 22:59:32 +0000

    feat(send): impement SocketSend
    This class is what we will use to send data over network, it implements
    context manager methods but I presume we will not use them

---
+++ model_tests/yolov5-deepsort/src/send.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import yaml
+import time
+from socket import socket, AF_INET, SOCK_STREAM, SHUT_RDWR
+from select import select
+
+
+from interfaces import Send, Martial
+from martial import JsonMartialler
+
+class SocketSend(Send):
+    def __init__(self, martialler: Martial, port, host = ''):
+        self.host = host
+        self.port = port
+        self.martialler = martialler
+
+        # socket creation
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.setblocking(True)
+        self.socket.bind((self.host, self.port))
+
+        self.socket.listen(1)
+        self.conn, self.addr = self.socket.accept()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *_):
+        self.close()
+
+    def send(self, data):
+        sendable = self.martialler.serialise(data)
+        print(f"conn on {self.addr}")
+        self.conn.sendall(sendable)
+
+    def close(self):
+        self.conn.shutdown(SHUT_RDWR)
+        self.socket.shutdown(SHUT_RDWR)
+
+        self.conn.close()
+        self.socket.close()
+
+if __name__ == '__main__':
+    with open('../config.yml' , 'r') as f:
+        config = yaml.safe_load(f)['yolov5_deepsort']['main']
+
+    with SocketSend(JsonMartialler(), config['port'], config['host']) as net2:
+        net2.send({"test": 3})
+
+    net = SocketSend(JsonMartialler(), config['port'], config['host'])
+
+    net.send({"hello": "world"})
+    time.sleep(1)
+    net.send({"data": "2"})
+
+    net.close()
+
